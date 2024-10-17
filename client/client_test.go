ac71fbaac06c10cf7e985d085ca8ef441f1a7b69
Goutham Muguluvalli Niranjan <goutham.mn034@gmail.com>
2024-10-17 13:14:41 -0500

    add interface to create remotePDP client client and update tests

--- client/client_test.go
+++ client/client_test.go
@@ -6,6 +6,7 @@ package client
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -19,13 +20,55 @@ import (
 func TestClientCreate(t *testing.T) {
 	endpoint := "https://westus.authorization.azure.net/providers/Microsoft.Authorization/checkAccess?api-version=2021-06-01-preview"
 	scope := "https://authorization.azure.net/.default"
+	emptyString := "   "
 	cred, err := azidentity.NewClientSecretCredential("888988bf-86f1-31ea-91cd-2d7cd011db48", "clientID", "clientSecret", nil)
 	if err != nil {
-		t.Error("Unable to create a new PDP client")
+		t.Error("Unable to create a new client secret credential")
 	}
-	client := NewRemotePDPClient(endpoint, scope, cred)
-	if client.endpoint != endpoint {
-		t.Error("The client endpoint doesn't equal to the target endpoint")
+
+	cases := []struct {
+		desc             string
+		endpoint         string
+		scope            string
+		cred             *azidentity.ClientSecretCredential
+		expectedEndpoint string
+		expectedErr      error
+	}{
+		{
+			desc:        "fail - Invalid endpoint",
+			endpoint:    emptyString,
+			scope:       scope,
+			cred:        cred,
+			expectedErr: fmt.Errorf("endpoint: %s is not valid, need a valid endpoint in creating client", emptyString),
+		}, {
+			desc:        "fail - Invalid scope",
+			endpoint:    endpoint,
+			scope:       emptyString,
+			cred:        cred,
+			expectedErr: fmt.Errorf("scope: %s is not valid, need a valid scope in creating client", emptyString),
+		}, {
+			desc:        "fail - Invalid credential",
+			endpoint:    endpoint,
+			scope:       scope,
+			cred:        nil,
+			expectedErr: fmt.Errorf("need TokenCredential in creating client"),
+		}, {
+			desc:             "success - successful creation of client",
+			endpoint:         endpoint,
+			scope:            scope,
+			cred:             cred,
+			expectedEndpoint: endpoint,
+			expectedErr:      nil,
+		},
+	}
+	for _, c := range cases {
+		client, err := NewRemotePDPClient(c.endpoint, c.scope, c.cred, nil)
+		if err != nil && err.Error() != c.expectedErr.Error() {
+			t.Errorf("%s: expected error to be '%v'. Got '%v'", c.desc, c.expectedErr, err)
+		}
+		if client != nil && client.endpoint != c.expectedEndpoint {
+			t.Errorf("%s: expected endpoint to be %s. Got %s", c.desc, c.expectedEndpoint, client.endpoint)
+		}
 	}
 }
 
